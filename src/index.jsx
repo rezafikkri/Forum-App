f9011bda589eaadd6e2c1d9bbda0c84764cb8186
rezafikkri <fikkri.reza@gmail.com>
2024-03-01 10:05:18 +0700

    chore: install bootstrap

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+// style
+import "./styles/style.css";
 
 const root = createRoot(document.getElementById('root'));
 root.render(
