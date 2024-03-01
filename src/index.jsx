2c222ac79de4ea3217c319b2bebc8e378b665f26
rezafikkri <fikkri.reza@gmail.com>
2024-03-01 10:07:45 +0700

    style: use single quote for string

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 // style
-import "./styles/style.css";
+import './styles/style.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
